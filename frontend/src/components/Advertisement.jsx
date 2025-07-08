c57a57b06542bbd394ddfcb4a171d9d18ea2966d
subbu-yadhati <166973925+subbu-yadhati@users.noreply.github.com>
2025-07-08 16:24:55 +0530

    Add files via upload

---
+++ frontend/src/components/Advertisement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import backgroundImage from "../assets/Rectangle 18.png";
+import smartphoneImage from "../assets/Isolated_right_hand_with_smartphone 2.png";
+
+const Advertisement = () => {
+  return (
+    <div
+      className="relative w-[94%] mx-auto flex items-center justify-center mt-14"
+    >
+    </div>
+  );
+};
+
+export default Advertisement;
